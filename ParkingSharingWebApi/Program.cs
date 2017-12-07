9493c06d9de0d698f8b2533059a177ef7ad7c48a
Alius Petraska <alius.petraska@ibm.com>
2017-12-07 12:21:22 +0200

    packages added;

--- ParkingSharingWebApi/Program.cs
+++ ParkingSharingWebApi/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace ParkingSharingWebApi
 {
@@ -19,7 +13,13 @@ namespace ParkingSharingWebApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                   .ConfigureAppConfiguration((builderContext, config) =>
+                   {
+                       // delete all default configuration providers
+                       config.Sources.Clear();
+                       config.AddJsonFile("vcap-local.json", optional: true);
+                   })
+                    .UseStartup<Startup>()
+                    .Build();
     }
 }
--- ParkingSharingWebApi/Startup.cs
+++ ParkingSharingWebApi/Startup.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace ParkingSharingWebApi
 {
@@ -16,6 +15,25 @@ namespace ParkingSharingWebApi
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            var vcapServices = Environment.GetEnvironmentVariable("VCAP_SERVICES");
+
+            if (vcapServices != null)
+            {
+                dynamic json = JsonConvert.DeserializeObject(vcapServices);
+
+                if (json["compose-for-mysql"] != null)
+                {
+                    try
+                    {
+                        Configuration["compose-for-mysql:0:credentials:uri"] = json["compose-for-mysql"][0].credentials.uri;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Failed to read Compose for MySQL uri, ignore this and continue without a database.");
+                    }
+                }
+            }
         }
 
         public IConfiguration Configuration { get; }
@@ -23,7 +41,45 @@ namespace ParkingSharingWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var databaseUri = Configuration["compose-for-mysql:0:credentials:uri"];
+
+            if (!string.IsNullOrEmpty(databaseUri))
+            {
+                services.AddDbContext<ParkingSharingContext>(options => options.UseMySql(
+                    GetConnectionString(databaseUri)));
+
+                services.AddDataProtection()
+                    .SetDefaultKeyLifetime(TimeSpan.FromDays(365));
+            }
+
             services.AddMvc();
+        }
+
+        static string GetConnectionString(string databaseUri)
+        {
+            string connectionString;
+
+            try
+            {
+                var username = databaseUri.Split('/')[2].Split(':')[0];
+                var password = databaseUri.Split(':')[2].Split('@')[0];
+                var portSplit = databaseUri.Split(':');
+                var port = portSplit.Length == 4 ? portSplit[3].Split('/')[0] : null;
+                var hostSplit = databaseUri.Split('@')[1];
+                var hostname = port == null ? hostSplit.Split('/')[0] : hostSplit.Split(':')[0];
+                var databaseSplit = databaseUri.Split('/');
+                var database = databaseSplit.Length == 4 ? databaseSplit[3] : null;
+                var optionsSplit = database?.Split('?');
+                database = optionsSplit.First();
+                port = port ?? "3306"; // if port is null, use 3306
+                connectionString = $"Server={hostname};uid={username};pwd={password};Port={port};Database={database};SSL Mode=Required;";
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new FormatException("Invalid database uri format", ex);
+            }
+
+            return connectionString;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +89,10 @@ namespace ParkingSharingWebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/static-files
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseMvc();
         }
